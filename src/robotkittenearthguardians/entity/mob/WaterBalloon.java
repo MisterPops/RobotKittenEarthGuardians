82d902cf167c8809eeb318fd79f997d5da6f073c
Brandon Feist <feist.brandon@gmail.com>
2013-11-15 00:04:02 -0800

    Messed around with waterballoon's speed...goodnight.

--- src/robotkittenearthguardians/entity/mob/WaterBalloon.java
+++ src/robotkittenearthguardians/entity/mob/WaterBalloon.java
@@ -7,7 +7,7 @@ import robotkittenearthguardians.level.Level;
 
 public class WaterBalloon extends Mob{
 
-	private double speed = 2.5;
+	private double speed = 3.2;
 	private int sightRange = 300;
 	Ai ai;
 

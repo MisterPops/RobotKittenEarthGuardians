bc6c4dc3ea80326e73fedb3a7dfa2b126008ece0
Brandon Feist <feist.brandon@gmail.com>
2013-12-03 21:50:55 -0800

    Just Testing mob.

--- src/robotkittenearthguardians/entity/mob/WaterBalloon.java
+++ src/robotkittenearthguardians/entity/mob/WaterBalloon.java
@@ -10,7 +10,7 @@ import robotkittenearthguardians.level.Level;
 public class WaterBalloon extends Mob{
 
 	private double speed = 3.2;
-	private int sightRange = 250;
+	private int sightRange = 300;
 	WaterBalloonAi ai;
 
 	public WaterBalloon(int x, int y) {
